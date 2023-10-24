a19c98db0cef8c2df53eee04cfeab32a2780a61a
mpanne <merlin.pannewitz@digitalservice.bund.de>
2023-10-24 14:12:01 +0200

    Add e2e tests with playwright

---
+++ tests/e2e/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("basic example test", () => {
+
+  test('has title', async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle("Remix Application Template");
+  });
+
+  test("shows hello message", async ({ page }) => {
+    await page.goto("/")
+    await expect(page.locator("text=DigitalService")).toBeVisible()
+  })
+
+})
