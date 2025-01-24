23f8dd628606ba478ba53c2eefbfcb23fe73b4a2
chris <christoph.hohnerlein@digitalservice.bund.de>
2025-01-24 15:06:01 +0100

    Chore(deps): upgrade to remix 2.15.2

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ import tsconfigPaths from "vite-tsconfig-paths";
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
   server: { port: 3000 },
 });
